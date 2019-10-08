0e14a53ad162c4ec04bef0f7e533f570bc201138
svogeti <sameervogeti@gmail.com>
2019-10-08 15:13:22 +0530

    Adding Skeleton for spring-cloud-config-server , closes #1

---
+++ spring-cloudf-config-server/src/main/java/com/currencyconverter/microservices/springcloudfconfigserver/SpringCloudfConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.currencyconverter.microservices.springcloudfconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudfConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudfConfigServerApplication.class, args);
+    }
+
+}
