d583adfc1aaabafa60ee2b1e05296f2a78cb698a
svogeti <sameervogeti@gmail.com>
2019-10-09 12:21:01 +0530

    Added JPA support and created entities, closes #9

--- currency-exchange-service/src/main/java/com/currencyconverter/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/currencyconverter/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,6 +1,9 @@
 package com.currencyconverter.microservices.currencyexchangeservice.controller;
 
 import com.currencyconverter.microservices.currencyexchangeservice.model.ExchangeValue;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,14 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 import java.math.BigDecimal;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/currency-exchange")
 public class CurrencyExchangeController {
+
+    @Autowired
+    Environment environment;
 
     @GetMapping("/from/{from}/to/{to}")
     public ExchangeValue retrieveExchangeValue(@PathVariable("from") String from,
                                                @PathVariable("to") String to) {
 
-        return new ExchangeValue(1l, from, to, new BigDecimal(65));
+        Integer port = Integer.valueOf(environment.getProperty("local.server.port"));
+        return new ExchangeValue(1l, from, to, new BigDecimal(65), port);
     }
 
 }
--- currency-exchange-service/src/main/java/com/currencyconverter/microservices/currencyexchangeservice/model/ExchangeValue.java
+++ currency-exchange-service/src/main/java/com/currencyconverter/microservices/currencyexchangeservice/model/ExchangeValue.java
@@ -4,14 +4,22 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Entity
 public class ExchangeValue {
+    @Id
     private Long id;
+    @Column(name = "currency_from")
     private String from;
+    @Column(name = "currency_to")
     private String to;
     private BigDecimal conversionMultiple;
+    private Integer port;
 }
