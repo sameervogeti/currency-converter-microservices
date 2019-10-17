238eddf51aa2883f247811d1630849f9e38c3e73
svogeti <sameervogeti@gmail.com>
2019-10-17 18:59:06 +0530

    Routing calls through Zuul API Gateway, closes #21

--- currency-conversion-service/src/main/java/com/currencyconverter/microservices/currencyconversionservice/service/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/currencyconverter/microservices/currencyconversionservice/service/CurrencyExchangeServiceProxy.java
@@ -6,11 +6,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange-service")
-@RibbonClient(name = "currency-exchange-service")
+@FeignClient(name = "netflix-zuul-api-gateway-server")
+@RibbonClient(name = "netflix-zuul-api-gateway-server")
 public interface CurrencyExchangeServiceProxy {
 
-    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    @GetMapping("currency-exchange-service/currency-exchange/from/{from}/to/{to}")
     public CurrencyConversionBean retrieveExchangeValue(@PathVariable String from,
                                                         @PathVariable String to);
 }
